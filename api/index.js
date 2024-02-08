ae672f2fff282dc12d0b7bb92e86bd3e5cc5846e
sana shahzad <as1526314@gmail.com>
2024-02-08 21:33:55 +0500

    first

--- api/index.js
+++ api/index.js
@@ -38,6 +38,7 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.json("Hello");
 })
 
 app.use((err, req, res, next) => {
